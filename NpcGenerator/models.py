f8a03a194fdadfb3961032aeb7fc6d32ba262648
adam.kowalski <adam.kowalski@intive.com>
2020-01-20 17:38:54 +0100

    add new models to model.py and to admin site

--- NpcGenerator/models.py
+++ NpcGenerator/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
+class Race(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Subclass(models.Model):
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    hero_class = models.ForeignKey(Class, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name

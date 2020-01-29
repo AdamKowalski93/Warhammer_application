d92137d438394d08afec3e2ca5e0adca92349630
adam.kowalski <adam.kowalski@intive.com>
2020-01-29 14:12:22 +0100

    Create first serializer with list of Races

--- NpcGenerator/views.py
+++ NpcGenerator/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Race
+from rest_framework import viewsets
+from NpcGenerator.serializers import RaceSerializer
 
-# Create your views here.
+
+class RaceViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Race.objects.all()
+    serializer_class = RaceSerializer
